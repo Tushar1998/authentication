13d9ab7ad706203bd5b80d642b408d6e495a7159
Tushar Mistry <tusharmistry@MacBook-Air.local>
2020-12-10 15:44:29 +0530

    protect route error solved

--- middlewares/protectRoute.js
+++ middlewares/protectRoute.js
@@ -1,44 +1,48 @@
 const fs = require('fs');
 const path = require('path');
+
 const AppError = require('../helpers/appError.js');
-
-const fileName = path.join(__dirname, "..", "data", "users.json");
-const users = JSON.parse(fs.readFileSync(fileName), 'utf-8');
-
 const { verifyToken } = require('../helpers/jwtAuthentication.js');
 const sendErrorMessage = require('../helpers/sendError.js');
 
+const fileName = path.join(__dirname, "..", "data", "users.json");
+const users = JSON.parse(fs.readFileSync(fileName, "utf-8"));
 const protectRoute = async (req, res, next) => {
-
-    if (!req.header.authorization) {
-        return sendErrorMessage(new AppError(401, "Unsucessfull", "Not logged in"), req, res);
+    //   console.log("headers in req body", req.headers.authorization);
+    // extract token
+    if (!req.headers.authorization) {
+        return sendErrorMessage(
+            new AppError(401, "Unsuccessful", "Please login or signup"),
+            req,
+            res,
+        );
     }
-
-    let jwtToken = req.headers.authorization.split(' ')[1];
-
+    // if headers are there
+    let jwtToken = req.headers.authorization.split(" ")[1];
+    let decoded;
     try {
-        let decoded = await verifyToken(jwtToken, process.env.JWT_SECRET);
-
-        req.body.email == decoded.email;
-
-        let currentUser = users.find(user => {
-            return user.email == decoded.email;
-        })
-
-        // console.log(jwtToken);
-
-
-        if (!currentUser) {
-            sendErrorMessage(new AppError(401, "Unsucessfull", "Not logged in"), req, res)
-            return res.send()
-        }
-
-        req.currentUser = currentUser;
-        next();
+        decoded = await verifyToken(jwtToken, process.env.JWT_SECRET);
     } catch (err) {
-        return new AppError(err, req, res);
+        return sendErrorMessage(
+            new AppError(401, "Unsuccesssul", "Invalid Token"),
+            req,
+            res,
+        );
     }
-
-}
+    let { email: currentUser } = users.find((user) => {
+        return user.email == decoded.email;
+    });
+    if (!currentUser) {
+        return sendErrorMessage(
+            new AppError(401, "Unsuccesssul", "User not registered"),
+            req,
+            res,
+        );
+    }
+    // check verification
+    req.currentUser = currentUser;
+    // give access
+    next();
+};
 
 module.exports = protectRoute;
