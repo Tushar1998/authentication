d90f6cb5b1730d56294e76e752d2ed20cf646335
Tushar Mistry <mistry.tushar98@gmail.com>
2021-01-07 00:34:26 +0530

    login incomplete

--- controllers/userMongoControllers.js
+++ controllers/userMongoControllers.js
@@ -5,54 +5,54 @@ const uniqid = require("uniqid");
 const User = require("../models/userMongoModel");
 
 const signUpUser = async (req, res) => {
-	try {
-		let newUser = new User({
-			userId: uniqid(),
-			email: req.body.email,
-			password: req.body.password,
-		});
-		let result = await newUser.save();
-		res.json({
-			status: "SucessFull",
-			message: "User Created",
-			user: newUser,
-		});
-	} catch (error) {
-		console.log(error);
-		res.json({
-			status: "UnSucessfull",
-			message: error,
-		});
-	}
+  try {
+    let newUser = new User({
+      userId: uniqid(),
+      email: req.body.email,
+      password: req.body.password,
+    });
+    let result = await newUser.save();
+    res.json({
+      status: "SucessFull",
+      message: "User Created",
+      user: newUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: "UnSucessfull",
+      message: error,
+    });
+  }
 };
 
 const loginUser = async (req, res) => {
-	try {
-		let result = await bcrypt.compare(req.body.password, req.currentUser.password);
-		let jwtToken = await generateToken({ email: req.currentUser.email }, process.env.JWT_SECRET, {
-			expiresIn: "1d",
-		});
+  try {
+    // let result = await bcrypt.compare(req.body.password, req.currentUser.password);
+    let result = await bcrypt.compare(req.body.password, findUser.password);
+    let jwtToken = await generateToken({ email: findUser.email }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.cookie("jwt", jwtToken);
+    res.status(200).json({
+      status: "Sucessfull",
+      data: [{ jwt: jwtToken }],
+    });
+    if (!result) {
+      sendErrorMessage(new AppError(401, "Unsucessfull", "Password is incorrect"), req, res);
+    }
 
-		res.cookie("jwt", jwtToken);
-		res.status(200).json({
-			status: "Sucessfull",
-			data: [{ jwt: jwtToken }],
-		});
-		if (!result) {
-			sendErrorMessage(new AppError(401, "Unsucessfull", "Password is incorrect"), req, res);
-		}
-
-		res.send("User logged in Sucessfully");
-	} catch (err) {
-		console.log(err);
-		res.json({
-			status: "Unsucessfull",
-		});
-		// sendErrorMessage(new AppError(401, "Unsucessfull", "Password is incorrect"), req, res);
-	}
+    res.send("User logged in Sucessfully");
+  } catch (err) {
+    console.log(err);
+    res.json({
+      status: "Unsucessfull",
+    });
+    // sendErrorMessage(new AppError(401, "Unsucessfull", "Password is incorrect"), req, res);
+  }
 };
 
 module.exports = {
-	signUpUser,
-	loginUser,
+  signUpUser,
+  loginUser,
 };
--- middlewares/userMongoMiddlewares.js
+++ middlewares/userMongoMiddlewares.js
@@ -1,13 +1,14 @@
 const User = require("../models/userMongoModel");
 
 const isUserRegistered = async (req, res, next) => {
-	try {
-		let findUser = await User.findOne({ email: req.body.email });
-		console.log(findUser);
-		next();
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    let findUser = await User.findOne({ email: req.body.email });
+    // console.log(findUser);
+    // req.currentUser = { ...findUser };
+    next(findUser);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = isUserRegistered;
